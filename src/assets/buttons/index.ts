68de4f7620733ffbaece4bbc0a09a497b77061aa
calvinham <calvinjunham@gmail.com>
2023-07-11 19:02:13 -0600

    add rsvp images

--- src/assets/buttons/index.ts
+++ src/assets/buttons/index.ts
@@ -1,7 +1,16 @@
 import nextIconImg from './Next.svg';
 import rsvpIconImg from './RSVP.svg';
+import YesIconImg from './Yes.svg';
+import NoIconImg from './No.svg';
 
 const nextButtonTextImg = nextIconImg;
 const rsvpButtonTextImg = rsvpIconImg;
+const yesButtonTextImg = YesIconImg;
+const noButtonTextImg = NoIconImg;
 
-export { nextButtonTextImg, rsvpButtonTextImg };
+export {
+  nextButtonTextImg,
+  rsvpButtonTextImg,
+  yesButtonTextImg,
+  noButtonTextImg,
+};
